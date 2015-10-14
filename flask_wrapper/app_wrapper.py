6a3cf3a9992c8934218f7e10d021e4bf90e78857
Christopher Su <christophersu9@gmail.com>
2015-10-13 23:18:53 -0700

    comment

--- flask_wrapper/app_wrapper.py
+++ flask_wrapper/app_wrapper.py
@@ -17,6 +17,7 @@ class FlaskAppWrapper:
     def add_routes(self, module, route_map):
         for r, m in route_map.iteritems():
             method = getattr(module, m)
+            # methods must use flask's request api themselves
             self.app.add_url_rule(r, method.__name__, method)
 
     def run(self):
